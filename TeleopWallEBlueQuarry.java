736a9bf885595b6f917f7b38c06ce16131c8a1fc
FTC10309 <35548622+FTC10309@users.noreply.github.com>
2019-12-08 15:52:36 -0500

    Add files via upload

---
+++ TeleopWallEBlueQuarry.java
@@ -0,0 +1,60 @@
+ package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by qiu29 on 9/27/2019.
+ */
+
+ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+ import static org.firstinspires.ftc.teamcode.odometry.OdoGPS.ENCODER_PER_INCH;
+ import static org.firstinspires.ftc.teamcode.odometry.OdoGPS.O_INDEX;
+ import static org.firstinspires.ftc.teamcode.odometry.OdoGPS.X_INDEX;
+ import static org.firstinspires.ftc.teamcode.odometry.OdoGPS.Y_INDEX;
+
+ /**
+ * Opmode for TeleOp operation
+ * The following are the full list of controls available to driver (lift is front of the robot)
+ *  */
+
+@TeleOp(name="TeleOp Quarry Blue", group="Blue Linear Opmode")
+//@Disabled
+public class TeleopWallEBlueQuarry extends LinearOpMode {
+     // Declare OpMode members.
+     private SkyStoneJediBotTeleop r = new SkyStoneJediBotTeleop();
+     @Override
+     public void runOpMode() {
+         r.period.reset();
+         r.init(hardwareMap);
+         r.setOP(this,null,true);
+         r.initOpmode();
+         // Wait for the game to start (driver presses PLAY)
+         while (!isStarted() && !isStopRequested()){
+             telemetry.addData("Have been waiting ", r.period.seconds());
+             telemetry.addData("X Position", r.gps.odoData[X_INDEX] / ENCODER_PER_INCH);
+             telemetry.addData("Y Position", r.gps.odoData[Y_INDEX] / ENCODER_PER_INCH);
+             telemetry.addData("Orientation (Radian)", r.gps.odoData[O_INDEX]);
+             telemetry.addData( "Number stones already pickes",r.nextStone);
+             telemetry.addData("skyOrder is ", r.skyOrder);
+             telemetry.update();
+         }
+         r.period.reset();
+         // run until the end of the match (driver presses STOP)
+         while (opModeIsActive()) {
+             r.getSensors();
+             r.moveWallEServo();
+             r.moveSlide();
+             r.checkDriveTrain();
+             r.pickUpStone();
+             r.quarrySetState();
+             r.quarryReturnState();
+             r.flipStone();
+             r.moveCapstone();
+             telemetry.addData("X Position", r.gps.odoData[X_INDEX] / ENCODER_PER_INCH);
+             telemetry.addData("Y Position", r.gps.odoData[Y_INDEX] / ENCODER_PER_INCH);
+             telemetry.addData("Orientation (Radian)", r.gps.odoData[O_INDEX]);
+             telemetry.addData("Lift encoder", r.liftPos);
+             telemetry.addData("Collect encoder", r.collectPos);
+             telemetry.update();
+         }
+     }
+}
---
+++ TeleopWallERedBuild.java
@@ -0,0 +1,93 @@
+ package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by qiu29 on 9/27/2018.
+ */
+
+ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+ import com.qualcomm.robotcore.hardware.DcMotor;
+ import com.qualcomm.robotcore.util.ReadWriteFile;
+
+ import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+ import org.firstinspires.ftc.teamcode.revExtensions.RevBulkData;
+
+ import java.io.File;
+
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.AUTO_MIN_POWER;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.CAPSTONE_IN;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.CAPSTONE_OUT;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.COLLECT_ENCODER_PER_INCH;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.FOUNDATIONMOVER2DOWN;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.FOUNDATIONMOVER2HIGHLIMIT;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.FOUNDATIONMOVER2LOWLIMIT;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.FOUNDATIONMOVER2MIDDLE;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.FOUNDATIONMOVERDOWN;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.FOUNDATIONMOVERHIGHLIMIT;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.FOUNDATIONMOVERLOWLIMIT;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.FOUNDATIONMOVERMIDDLE;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.LIFT_ENCODER_PER_RADIAN;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.MAX_COLLECT_ENCODER;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.MAX_LIFT_ENCODER;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.MAX_P;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.MIN_LIFT_ENCODER;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.PICK_UP;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.P_INCREASE;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.ROTATE_BLUE_BACK;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.ROTATE_NEUTRAL;
+ import static org.firstinspires.ftc.teamcode.HardwareWallEbot.STOP_P;
+ import static org.firstinspires.ftc.teamcode.odometry.OdoGPS.ENCODER_PER_INCH;
+ import static org.firstinspires.ftc.teamcode.odometry.OdoGPS.O_INDEX;
+ import static org.firstinspires.ftc.teamcode.odometry.OdoGPS.X_INDEX;
+ import static org.firstinspires.ftc.teamcode.odometry.OdoGPS.Y_INDEX;
+
+ /**
+ * Opmode for TeleOp operation
+ * The following are the full list of controls available to driver (lift is front of the robot)
+ *
+*/
+@TeleOp(name="TeleOp Build Red", group="Red Linear Opmode")
+//@Disabled
+public class TeleopWallERedBuild extends LinearOpMode {
+
+    // Declare OpMode members.
+    private SkyStoneJediBotTeleop r = new SkyStoneJediBotTeleop();
+    @Override
+    public void runOpMode() {
+        r.period.reset();
+        r.init(hardwareMap);
+        r.setOP(this,null,false);
+        r.setBuild();
+        r.initOpmode();
+        r.setBuild();
+// Wait for the game to start (driver presses PLAY)
+        while (!isStarted() && !isStopRequested()){
+            telemetry.addData("Have been waiting ", r.period.seconds());
+            telemetry.addData("X Position", r.gps.odoData[X_INDEX] / ENCODER_PER_INCH);
+            telemetry.addData("Y Position", r.gps.odoData[Y_INDEX] / ENCODER_PER_INCH);
+            telemetry.addData("Orientation (Radian)", r.gps.odoData[O_INDEX]);
+            telemetry.addData( "Number stones already pickes",r.nextStone);
+            telemetry.addData("skyOrder is ", r.skyOrder);
+            telemetry.update();
+        }
+        r.period.reset();
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            r.getSensors();
+            r.moveWallEServo();
+            r.moveSlide();
+            r.checkDriveTrain();
+            r.pickUpStone();
+            r.buildSetState();
+            r.buildReturnState();
+            r.flipStone();
+            r.moveCapstone();
+            telemetry.addData("X Position", r.gps.odoData[X_INDEX] / ENCODER_PER_INCH);
+            telemetry.addData("Y Position", r.gps.odoData[Y_INDEX] / ENCODER_PER_INCH);
+            telemetry.addData("Orientation (Radian)", r.gps.odoData[O_INDEX]);
+            telemetry.addData("Lift encoder", r.liftPos);
+            telemetry.addData("Collect encoder", r.collectPos);
+            telemetry.update();
+        }
+    }
+}
